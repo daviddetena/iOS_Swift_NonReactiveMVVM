827dc11e04cf1a2ab3a6de4aec02d45a95742754
David de Tena <daviddetenacalvo@gmail.com>
2021-03-11 21:45:46 +0100

    Folder structured added. README updated.

---
+++ Non Reactive MVVM/Models/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Non Reactive MVVM
+//
+//  Created by David de Tena Calvo on 11/3/21.
+//
+
+import Foundation
+
+struct Friend {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let imageSmall: String
+    let imageLarge: String
+    let about: String
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#MVVM: A non-reactive introduction
+# MVVM: A non-reactive introduction
 
 This is just a fork from the [Ian Keen's repository - MVVM: A non-reactive introduction](https://github.com/IanKeen/NonReactiveMVVM) that accompanies his [blog post](https://medium.com/@IanKeen/mvvm-a-non-reactive-introduction-5a1f33402a32).
 
