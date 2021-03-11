e74ca15271e3eedd241b8ce2551c72213a079062
David de Tena <daviddetenacalvo@gmail.com>
2021-03-11 20:59:03 +0100

    Initial Commit

---
+++ Non Reactive MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Non Reactive MVVM
+//
+//  Created by David de Tena Calvo on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
